7a47304cca493a94b8c8cc151cb872b76af9f2b9
malekwessam <96774036+malekwessam@users.noreply.github.com>
2023-02-09 02:18:01 +0200

    Add files via upload

---
+++ School/CustomHtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School
+{
+    public static class CustomHtmlHelpers
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string alt, string width, string height)
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.MergeAttribute("src", VirtualPathUtility.ToAbsolute(src));
+            builder.MergeAttribute("alt", alt);
+            builder.MergeAttribute("width", width);
+            builder.MergeAttribute("height", height);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
